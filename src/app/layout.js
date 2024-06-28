1d1c0f868d016d54c755e263acf42c281a44bfb5
Muskaan Shaikh <muskaan.m.y.shaikh@gmail.com>
2024-06-28 00:37:28 -0400

    Changed Icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Muskaan Shaikh",
-  description: "Portfolio website, Software Engineer, Software Developer, Web Developer, Frontend Developer",
+  description: "Software Developer",
 };
 
 export default function RootLayout({ children }) {
